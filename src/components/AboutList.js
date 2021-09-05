c53da963b8d6c401b24ebac42e936ee8f0416e10
Simon Gagnon <gagnonbsimon@gmail.com>
2021-09-05 10:47:11 -0400

    finished about mobile

--- src/components/AboutList.js
+++ src/components/AboutList.js
@@ -3,7 +3,9 @@ import React from 'react'
 function AboutList(props) {
     return (
         <div className="about__item">
-            <img src={props.image} alt="" />
+            <div className="about__item__img">
+                <img src={props.image} alt="" />
+            </div>
             <h2>{props.title}</h2>
             <p>{props.description}</p>
         </div>
