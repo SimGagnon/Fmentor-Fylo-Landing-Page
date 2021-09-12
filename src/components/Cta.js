b953da12573102ed2d225f1d9d81c1e3438072ad
Simon Gagnon <gagnonbsimon@gmail.com>
2021-09-12 10:23:25 -0400

    fixed some footer margins

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -8,6 +8,11 @@ function Cta() {
         console.log("clicked");
     }
 
+    function validateEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
     return (
         <section className="cta">
             <div className="cta__container">
